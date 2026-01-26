943fce19933dad3a0dfdc36dfe727b643319df81
Tim Löffler <tim.justus.loeffler@gmail.com>
2026-01-26 16:37:16 +0100

    (feat) Adds GTK Theming

--- dotfiles/themes/.config/themes/apply-theme.sh
+++ dotfiles/themes/.config/themes/apply-theme.sh
@@ -21,6 +21,8 @@ fi
 
 ln -s "$THEME_DIR" "$CURRENT_LINK"
 
+echo "ln -s "$THEME_DIR" "$CURRENT_LINK""
+
 
 # WALLPAPER=$(find "$THEME_DIR" -maxdepth 1 -type f \( -iname "wallpaper.*" -o -iname "wallpaper" \) | head -n 1)
 
@@ -54,14 +56,34 @@ jq \
   '.["workbench.colorTheme"] = $theme' \
   "$SETTINGS" > "$tmp" && mv "$tmp" "$SETTINGS"
 
+# === Apply GTK Theme ===
+GTK_THEME="$(cat "$CURRENT_LINK/gtkTheme")"
+GTK_THEME_FOLDER="$HOME/.themes"
+gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
+
+mkdir -p "$GTK_THEME_FOLDER"
+GTK_CURRENT_LINK="$GTK_THEME_FOLDER/current"
+if [ -L "$GTK_CURRENT_LINK" ] || [ -e "$GTK_CURRENT_LINK" ]; then
+  rm -rf "$GTK_CURRENT_LINK"
+fi
+ln -s "$GTK_THEME_FOLDER/$GTK_THEME" "$GTK_CURRENT_LINK"
+
+rm -rf "$HOME/.config/gtk-4.0/gtk.css" "$HOME/.config/gtk-4.0/gtk-dark.css" "$HOME/.config/gtk-4.0/assets"
+
+ln -s "$GTK_THEME_FOLDER/$GTK_THEME/gtk-4.0/gtk.css" "$HOME/.config/gtk-4.0/gtk.css"
+ln -s "$GTK_THEME_FOLDER/$GTK_THEME/gtk-4.0/gtk-dark.css" "$HOME/.config/gtk-4.0/gtk-dark.css"
+ln -s "$GTK_THEME_FOLDER/$GTK_THEME/gtk-4.0/assets" "$HOME/.config/gtk-4.0/assets"
+
+
 hyprctl reload
 pkill hyprpaper
 hyprpaper &
 
 pkill -SIGUSR2 waybar || true
 pkill rofi || true
-pkill nautilus || true
+# pkill nautilus || true
 
 swaync-client --reload-config
+
 
 echo "Theme $THEME activated"
