84fc4e093ed8aad64226b5ebfa6dff037a156304
wurstcasesenario <Tim>
2026-02-15 21:51:39 +0100

    (fix) Switched to Bamubu Studio Flatpak Version

--- install/install-bambuStudio.sh
+++ install/install-bambuStudio.sh
@@ -2,4 +2,6 @@
 
 echo "=== Installing Bambu Studio ==="
 
-yay -S --needed --noconfirm bambustudio-bin
+# yay -S --needed --noconfirm bambustudio-bin
+
+flatpak install flathub com.bambulab.BambuStudio
